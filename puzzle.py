958eaa7037ed481557ca9fe3be4afcc3bf3cbd93
storrealba09 <37607767+storrealba09@users.noreply.github.com>
2020-10-29 15:16:48 -0700

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,17 +1,16 @@
+#Import Libraries
 import pandas as pd
 import plotly.express as px
-
+#Format floats on panda
 pd.options.display.float_format = '${:,.2f}'.format
+#Read and uppercase dataframe
 df = pd.read_csv('Data+for+TreefortBnB+Puzzle.csv')
 df['City'] = df['City'].str.upper()
 
+#Group Dataframe by City and aggregate the price mean
 grouped = df.groupby('City').agg({'$ Price':['mean']}).reset_index()
 
+#Visualization formatting and execution
 grouped.columns = ['City', 'Mean']
-
-print(grouped)
-
-grouped.to_csv('./grouped.csv')
-
 fig = px.bar(grouped, x = ('City'), y= ('Mean'))
 fig.show()
